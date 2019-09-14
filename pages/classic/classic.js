1385750f7de5f1dec4daed716aa39f8767e27d5e
sbuteryd@qq.com <sbuteryd@qq.com>
2019-09-14 15:00:32 +0800

    api rquest

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -12,7 +12,15 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.request({
+      url: 'http://bl.7yue.pro/v1/classic/latest',
+      header: {
+        appkey:'Htx4aD7sS0M9iX1j'
+      },
+      success:(res)=> {
+        console.log(res.data)
+      }
+    })
   },
 
   /**
